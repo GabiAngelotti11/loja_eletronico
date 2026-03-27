cca0a3ffcf31d0dac921df93910bc4db3cd10fee
Gabriel Angelotti <gabiangelotti11@gmail.com>
2026-03-27 17:29:49 -0300

    Index começado

---
+++ app.py
@@ -0,0 +1,20 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def pagina_inicial():
+    return render_template('index.html')
+
+@app.route('/login')
+def pagina_login():
+    return render_template('login.html')
+
+@app.route('/sobre')
+def pagina_sobre():
+    return render_template('sobre.html')
+
+
+if __name__ == '__main__':
+
+    app.run(debug=True)
